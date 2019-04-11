bdbfab1641d5446f9ffb14bc5ac7416b28e11b6f
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-04-11 22:38:30 +0200

    setup eslint work with prettier in vue project

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,12 +3,20 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:vue/essential',
+    '@vue/prettier',
+    'eslint:recommended',
+  ],
   rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'error',
   },
   parserOptions: {
     parser: 'babel-eslint',
   },
+  plugins: ['prettier'],
 };
