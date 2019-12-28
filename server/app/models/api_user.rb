6fec24a6e69d896ab491b1bf9ece4f3ebaa84971
sibsmc <siobhanmcloughlin3@gmail.com>
2019-12-28 18:02:29 +0100

    validates wish held by mentee

--- server/app/models/api_user.rb
+++ server/app/models/api_user.rb
@@ -1,20 +1,20 @@
 class ApiUser < ApplicationRecord
   # encrypt password
   #has_secure_password
-
+  
 
   # Can write model association here for example
   has_many :wishes, dependent: :destroy
   # validation
   validates_presence_of :email, :password_digest, :username
-  validates :email, uniqueness: true, on: :create and :update
+  validates :email, uniqueness: true, on: [:create, :update]
   validates :username, uniqueness: true, on: [:create, :update]
   validate :mentor_or_mentee
 
 
   def mentor_or_mentee
     if (mentor == false) && (mentee == false)
-      errors.add(:api_user, "user must either want to be a mentor or mentee or both")
+      errors.add(:api_user, "must either want to be a mentor or mentee or both")
     end
   end
 
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -7,12 +7,14 @@ users = ApiUser.create([
               {first_name: 'Bob', last_name: 'Dylan', city: 'New York', mentor: true, mentee: true, email: 'Test_email_2@example.com', password_digest: 'password2', username: 'user2'}, 
                {first_name: 'Jane', last_name: 'Smith', city: 'Edinburgh', mentor: false, mentee: true, email: 'Test_email3@example.com', password_digest: 'password3', username: 'user3' }
                ])
-Meetinginterval.create([{interval: 'weekly'}, {interval: 'daily'}])
-Programminglanguage.create([{language: 'python'},{language: 'SCALA'}])
+weekly_meet = Meetinginterval.create(interval: 'weekly')
+daily_meet =  Meetinginterval.create(interval: 'daily')
+python_lang = Programminglanguage.create(language: 'python')
+scala_lang =  Programminglanguage.create(language: 'SCALA')
 
-users.first.wishes.create([{available_offline: false, available_online: true, goal: 'Code real good', programminglanguage_id: Programminglanguage.second.id, meetinginterval_id: Meetinginterval.first.id}])
-users.second.wishes.create([{available_offline: true, available_online: true, goal: 'Get smarter', programminglanguage_id: Programminglanguage.first.id, meetinginterval_id: Meetinginterval.first.id}])
-users.first.wishes.create([{available_offline: false, available_online: true, goal: 'Code really well in Python', programminglanguage_id: Programminglanguage.first.id, meetinginterval_id: Meetinginterval.second.id}])
+users.first.wishes.create([{available_offline: false, available_online: true, goal: 'Code real good', programminglanguage: python_lang, meetinginterval: weekly_meet}])
+users.second.wishes.create([{available_offline: true, available_online: true, goal: 'Get smarter', programminglanguage: python_lang, meetinginterval: daily_meet}])
+users.first.wishes.create([{available_offline: false, available_online: true, goal: 'Code really well in Python', programminglanguage: scala_lang, meetinginterval: weekly_meet}])
 
 p "Created #{ApiUser.count} api_user entries"
 p "Created #{Meetinginterval.count} meeting interval entries"
