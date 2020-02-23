a0d757c64fa52c9d2ad6a62f96914df6e887746e
sibsmc <siobhanmcloughlin3@gmail.com>
2020-02-23 17:34:01 +0100

    removed should and made descriptions of custom validations more informative

--- server/spec/models/programminglanguage_spec.rb
+++ server/spec/models/programminglanguage_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Programminglanguage, type: :model do
-  it { should validate_presence_of(:language) }
+  it {is_expected.to validate_presence_of(:language) }
 end
