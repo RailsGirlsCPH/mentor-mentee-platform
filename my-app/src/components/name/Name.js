656bdf53814502590c1272f13e4aef97f75d8a8d
Laila1222 <korosililla@gmail.com>
2020-03-10 17:34:35 +0100

    added card component

---
+++ my-app/src/components/name/Name.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Name.css';
+
+const Name = ({name}) => {
+    return (
+        <div>
+            <h3>{name}</h3>
+        </div>
+    )
+}
+
+export default Name;
