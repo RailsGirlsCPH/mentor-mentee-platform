c3ba2592df5ed7b36d50d50a5810c005d1931891
Gizem Candemir <gizemc@gmail.com>
2020-03-12 11:15:09 +0100

    Clean all the weird lines from merge conflict

--- my-app/src/components/Card/Card.js
+++ my-app/src/components/Card/Card.js
@@ -3,20 +3,9 @@ import './Card.css';
 import Name from '../name/Name';
 import Avatar from '../avatar/avatarComponent';
 import Skill from '../skill/Skill.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Link from '../link/linkComponent.jsx';
 
 const Card = ({name, skills, link, text}) => {
-=======
-
-const Card = ({name, skills}) => {
->>>>>>> 3a357b11dc8b8c1e75cd9646d665bf452d3fe8ab
-=======
-import Link from '../link/linkComponent.jsx';
-
-const Card = ({name, skills, link, text}) => {
->>>>>>> 4cbe854a031e450f7944a89848d6ccb9c3160404
     return (
         <div className="card">
            <Avatar />
@@ -26,14 +15,7 @@ const Card = ({name, skills, link, text}) => {
             ? skills.map(skill => <Skill skills={skill} />)
             : <Skill skills={skills} />}
            </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
            <Link link={link} text={text} className='contact-link'/>
-=======
->>>>>>> 3a357b11dc8b8c1e75cd9646d665bf452d3fe8ab
-=======
-           <Link link={link} text={text} className='contact-link'/>
->>>>>>> 4cbe854a031e450f7944a89848d6ccb9c3160404
         </div>
     )
 }
--- my-app/src/components/link/linkComponent.jsx
+++ my-app/src/components/link/linkComponent.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import './link.css'
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4cbe854a031e450f7944a89848d6ccb9c3160404
 const Link = ({link, text}) => {
     return (
         <div>
             <a href={link}>{text}</a>
-<<<<<<< HEAD
-=======
-const Link = ({email}) => {
-    return (
-        <div>
-            <a href={`mailto:${email}`} className='contact-link'>Contact</a>
->>>>>>> 3a357b11dc8b8c1e75cd9646d665bf452d3fe8ab
-=======
->>>>>>> 4cbe854a031e450f7944a89848d6ccb9c3160404
         </div>
     )
 }
