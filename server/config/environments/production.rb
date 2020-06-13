eece32e645e4b26716b9781469df52bf66328a5a
sibsmc <siobhanmcloughlin3@gmail.com>
2020-06-13 13:08:28 +0200

    install devise and amend config env files for prod and dev

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -59,7 +59,19 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mentor_mentee_platform_production"
-
+  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  host = 'mentor-mentee-platform.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, port: 3000 }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => '587',
+  #   :authentication => :plain,
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :domain => 'heroku.com',
+  #   enable_starttls_auto => true
+  # }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
