5bb3793d3fe155515efa6a665c2ea237833ce23e
Siobhan <siobhanmcloughlin3@gmail.com>
2020-10-05 17:27:52 +0200

    CORS added to rails, react app can now sign up and get auth token back

--- server/config/application.rb
+++ server/config/application.rb
@@ -13,8 +13,14 @@ Bundler.require(*Rails.groups)
 module MentorMenteePlatform
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
 
+    config.load_defaults 5.1
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:80'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     #Handle temporary files in docker, want to avoid storing them as root on home machine.
 
     # Settings in config/environments/* take precedence over those specified here.
