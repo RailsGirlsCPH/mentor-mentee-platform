45c17e915dfc3768357dd7c53551b14025fb22b5
Siobhan <siobhanmcloughlin3@gmail.com>
2020-10-14 11:13:08 +0200

    created registration page to handle details passed to API

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { useState, useEffect } from "react"
+import "./App.css"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+function App() {
+    useEffect(() => {
+        const requestOptions = {
+            method: "POST",
+            mode: "cors",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                username: "ashley8",
+                email: "ashley8@email.com",
+                password: "foobar",
+                mentor: "true",
+                mentee: "true",
+            }),
+        }
+        fetch("http://localhost:3000/api/v1/signup", requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error))
+    })
+    return (
+        <div>
+            RailsGirls Mentor mentee platform, we can update on our machine in
+            October. we can make changes boo yah! Or can we though
+        </div>
+    )
+}
+
+export default App
