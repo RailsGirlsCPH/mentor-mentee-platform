b8f905b73783b2e79de14b16a12c61771d14ac55
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2020-11-01 16:12:00 +0100

    removed comments

--- my-app/src/components/button/button.component.js
+++ my-app/src/components/button/button.component.js
@@ -1,7 +1,3 @@
-// -----> I created this folder and file to show an example of
-// how you can make your component in storybook. <-----
-
-// Import react, css and other files if needed
 import React from "react";
 import "./button.css";
 
